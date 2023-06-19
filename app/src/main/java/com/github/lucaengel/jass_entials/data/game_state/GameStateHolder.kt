a4b2bfbcffcdecb2558ab855791096d6f159b7d1
LucaS <luca.engel@epfl.ch>
2023-06-19 15:05:58 +0200

    Jassround design

--- app/src/main/java/com/github/lucaengel/jass_entials/data/game_state/GameStateHolder.kt
+++ app/src/main/java/com/github/lucaengel/jass_entials/data/game_state/GameStateHolder.kt
@@ -1,8 +1,28 @@
 package com.github.lucaengel.jass_entials.data.game_state
 
+import com.github.lucaengel.jass_entials.data.cards.Deck
+import com.github.lucaengel.jass_entials.data.cards.Player
+import com.github.lucaengel.jass_entials.data.jass.Trump
+
 class GameStateHolder {
     companion object {
-        var gameState: GameState = GameState()
+        private val player1 = Player("email_1", 0, "first_1", "second_1", Deck.STANDARD_DECK.cards.subList(0, 9), 0, "123")
+        private val player2 = Player("email_2", 0, "first_2", "second_2", Deck.STANDARD_DECK.cards.subList(9, 18), 0, "123")
+        private val player3 = Player("email_3", 0, "first_3", "second_3", Deck.STANDARD_DECK.cards.subList(18, 27), 0, "123")
+        private val player4 = Player("email_4", 0, "first_4", "second_4", Deck.STANDARD_DECK.cards.subList(27, 36), 0, "123")
+
+
+        val currentPlayer = player1
+        var gameState: GameState = GameState(
+            listOf(player1, player2, player3, player4),
+            player1,
+            1,
+            mapOf(),
+            1,
+            Trump.UNGER_UFE,
+            Deck.STANDARD_DECK.dealCards(listOf(player1, player2, player3, player4))
+        )
+        //GameState()
         var bettingState: BettingState = BettingState()
     }
 }
--- app/src/main/java/com/github/lucaengel/jass_entials/game/JassRoundActivity.kt
+++ app/src/main/java/com/github/lucaengel/jass_entials/game/JassRoundActivity.kt
@@ -3,15 +3,37 @@ package com.github.lucaengel.jass_entials.game
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.github.lucaengel.jass_entials.data.cards.Deck
+import com.github.lucaengel.jass_entials.data.cards.Player
 import com.github.lucaengel.jass_entials.data.game_state.GameState
 import com.github.lucaengel.jass_entials.data.game_state.GameStateHolder
+import com.github.lucaengel.jass_entials.data.jass.Trump
 import com.github.lucaengel.jass_entials.ui.theme.JassentialsTheme
 
 class JassRoundActivity : ComponentActivity() {
@@ -20,27 +42,167 @@ class JassRoundActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         val gameState = GameStateHolder.gameState
-
-        println("gameState: $gameState")
 //        intent.getSerializableExtra("test", Test::class.java)
 
         setContent {
             JassentialsTheme {
                 Surface(color = MaterialTheme.colorScheme.background) {
-                    TableLayout()
+                    JassRoundPreview()
                 }
             }
         }
     }
 }
 
+@Preview(device = Devices.AUTOMOTIVE_1024p, widthDp = 720, heightDp = 360)
+//@Preview
 @Composable
-fun TableLayout() {
+fun JassRoundPreview() {
 
-    Box(
+    val player1 = Player("email_1", 0, "first_1", "second_1", Deck.STANDARD_DECK.cards.subList(0, 9), 0, "123")
+    val player2 = Player("email_2", 0, "first_2", "second_2", Deck.STANDARD_DECK.cards.subList(9, 18), 0, "123")
+    val player3 = Player("email_3", 0, "first_3", "second_3", Deck.STANDARD_DECK.cards.subList(18, 27), 0, "123")
+    val player4 = Player("email_4", 0, "first_4", "second_4", Deck.STANDARD_DECK.cards.subList(27, 36), 0, "123")
+
+
+    val currentPlayer = player1
+
+    JassentialsTheme {
+        Surface(color = MaterialTheme.colorScheme.background) {
+            JassRound(
+                currentPlayer,
+//                GameStateHolder.gameState
+                GameState(
+                    listOf(player1, player2, player3, player4),
+                    player1,
+                    1,
+                    mapOf(),
+                    1,
+                    Trump.UNGER_UFE,
+                    Deck.STANDARD_DECK.dealCards(listOf(player1, player2, player3, player4))
+                )
+            )
+        }
+    }
+}
+
+
+@Composable
+fun JassRound(
+    currentPlayer: Player,
+    gState: GameState,
+) {
+    val context = LocalContext.current
+    var currentPlayerIdx by remember { mutableStateOf(0) }
+
+
+    // REMOVE THIS ONCE LAYOUT IS FINE:
+//    val player1 = Player("email_1", 0, "first_1", "second_1", Deck.STANDARD_DECK.cards.subList(0, 9), 0, "123")
+//    val player2 = Player("email_2", 0, "first_2", "second_2", Deck.STANDARD_DECK.cards.subList(9, 18), 0, "123")
+//    val player3 = Player("email_3", 0, "first_3", "second_3", Deck.STANDARD_DECK.cards.subList(18, 27), 0, "123")
+//    val player4 = Player("email_4", 0, "first_4", "second_4", Deck.STANDARD_DECK.cards.subList(27, 36), 0, "123")
+//
+//    var state = BettingState(
+//            listOf(player1, player2, player3, player4),
+//            player1,
+//            JassTypes.SIDI_BARAHNI,
+//            listOf(
+//                Bet(player1, Trump.CLUBS, 40)
+//            ),
+//            GameState()
+//        )
+//    var bettingState by remember { mutableStateOf(state) }
+    // -----------------------------
+
+    var gameState by remember { mutableStateOf(GameStateHolder.gameState) }
+
+    LaunchedEffect(key1 = true) {
+        gameState = gState
+        currentPlayerIdx = gameState.players.indexOfFirst { it.email == currentPlayer.email }
+    }
+
+    Column(
         modifier = Modifier
             .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "Jass Round Activity!")
+
+        val topPlayer = gameState.players[(currentPlayerIdx + 2) % 4]
+
+        JassComposables.PlayerBox(player = topPlayer, playerSpot = 2)
+
+        Spacer(modifier = Modifier.weight(1f))
+
+
+        JassMiddleRowInfo(gameState = gameState, currentPlayerIdx = currentPlayerIdx)
+
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        JassComposables.CurrentPlayerBox(player = currentPlayer)
+    }
+}
+
+@Composable
+private fun JassMiddleRowInfo(gameState: GameState, currentPlayerIdx: Int) {
+    Row {
+
+        val leftPlayer = gameState.players[(currentPlayerIdx + 3) % 4]
+//        Box(
+//            modifier = Modifier
+//                .fillMaxHeight(0.5F)
+//                .fillMaxWidth(0.25F)
+//                .weight(1f),
+//            contentAlignment = Alignment.Center
+//        ) {
+        JassComposables.PlayerBox(
+            player = leftPlayer,
+            playerSpot = 3,
+            modifier = Modifier
+                .fillMaxHeight(0.5F)
+                .fillMaxWidth(0.25F)
+                .weight(1f))
+//        }
+
+        Spacer(modifier = Modifier.weight(0.1f))
+
+        Box(
+            modifier = Modifier
+                .align(Alignment.CenterVertically)
+                .background(
+                    MaterialTheme.colorScheme.surface,
+                    shape = RoundedCornerShape(10.dp)
+                )
+                .padding(5.dp)
+                .fillMaxWidth(0.5F)
+                .weight(1f),
+            contentAlignment = Alignment.Center
+        ) {
+
+//            if (gameState.bets.isEmpty()) {
+                Text(
+                    text = "No bets yet",
+                    textAlign = TextAlign.Center
+                )
+//            } else {
+//                val lastBet = gameState.bets.last()
+//
+//                Text(
+//                    text = "${lastBet.bet} ${lastBet.suit} by\n" +
+//                            "${lastBet.player.firstName} ${lastBet.player.lastName}",
+//                    textAlign = TextAlign.Center,
+//                    maxLines = 3,
+//                )
+//            }
+        }
+
+        Spacer(modifier = Modifier.weight(0.1f))
+
+        val rightPlayer = gameState.players[(currentPlayerIdx + 1) % 4]
+
+        JassComposables.PlayerBox(player = rightPlayer, playerSpot = 1, Modifier
+            .fillMaxHeight(0.5F)
+            .fillMaxWidth(0.25F)
+            .weight(1f))
     }
 }
