4739b1e8815e0315dcf71ae320bd20b4d58f5a42
LucaS <luca.engel@epfl.ch>
2023-06-20 12:11:48 +0200

    refactor

--- app/src/main/java/com/github/lucaengel/jass_entials/game/JassRoundActivity.kt
+++ app/src/main/java/com/github/lucaengel/jass_entials/game/JassRoundActivity.kt
@@ -27,7 +27,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
@@ -38,7 +37,6 @@ import com.github.lucaengel.jass_entials.data.cards.Deck
 import com.github.lucaengel.jass_entials.data.cards.Player
 import com.github.lucaengel.jass_entials.data.game_state.GameState
 import com.github.lucaengel.jass_entials.data.game_state.GameStateHolder
-import com.github.lucaengel.jass_entials.data.jass.Trump
 import com.github.lucaengel.jass_entials.ui.theme.JassentialsTheme
 
 class JassRoundActivity : ComponentActivity() {
@@ -77,19 +75,7 @@ fun JassRoundPreview() {
         Surface(color = MaterialTheme.colorScheme.background) {
             JassRound(
                 currentPlayer,
-//                GameStateHolder.gameState
-                GameState(
-                    players,
-                    player1,
-                    player3,
-                    1,
-                    players.mapIndexed { index, player ->
-                        player to Deck.STANDARD_DECK.cards[index]
-                    }.toMap(),
-                    1,
-                    Trump.UNGER_UFE,
-                    Deck.STANDARD_DECK.dealCards(players),
-                )
+                GameStateHolder.gameState
             )
         }
     }
@@ -101,27 +87,7 @@ fun JassRound(
     currentPlayer: Player,
     gState: GameState,
 ) {
-    val context = LocalContext.current
     var currentPlayerIdx by remember { mutableStateOf(0) }
-
-
-    // REMOVE THIS ONCE LAYOUT IS FINE:
-//    val player1 = Player("email_1", 0, "first_1", "second_1", Deck.STANDARD_DECK.cards.subList(0, 9), 0, "123")
-//    val player2 = Player("email_2", 0, "first_2", "second_2", Deck.STANDARD_DECK.cards.subList(9, 18), 0, "123")
-//    val player3 = Player("email_3", 0, "first_3", "second_3", Deck.STANDARD_DECK.cards.subList(18, 27), 0, "123")
-//    val player4 = Player("email_4", 0, "first_4", "second_4", Deck.STANDARD_DECK.cards.subList(27, 36), 0, "123")
-//
-//    var state = BettingState(
-//            listOf(player1, player2, player3, player4),
-//            player1,
-//            JassTypes.SIDI_BARAHNI,
-//            listOf(
-//                Bet(player1, Trump.CLUBS, 40)
-//            ),
-//            GameState()
-//        )
-//    var bettingState by remember { mutableStateOf(state) }
-    // -----------------------------
 
     var gameState by remember { mutableStateOf(GameStateHolder.gameState) }
 
@@ -137,15 +103,10 @@ fun JassRound(
     ) {
 
         val topPlayer = gameState.players[(currentPlayerIdx + 2) % 4]
-
         JassComposables.PlayerBox(player = topPlayer, playerSpot = 2)
 
         Spacer(modifier = Modifier.weight(1f))
-
-
         JassMiddleRowInfo(gameState = gameState, currentPlayerIdx = currentPlayerIdx)
-
-
         Spacer(modifier = Modifier.weight(1f))
 
         JassComposables.CurrentPlayerBox(player = currentPlayer)
@@ -157,29 +118,27 @@ private fun JassMiddleRowInfo(gameState: GameState, currentPlayerIdx: Int) {
     Row() {
 
         val leftPlayer = gameState.players[(currentPlayerIdx + 3) % 4]
-
         JassComposables.PlayerBox(
             player = leftPlayer,
             playerSpot = 3,
             modifier = Modifier
                 .fillMaxHeight(0.5F)
                 .fillMaxWidth(0.25F)
-                .weight(1f))
+                .weight(1f)
+        )
 
         Spacer(modifier = Modifier.weight(0.1f))
-
-
         CurrentTrick(gameState = gameState, currentPlayerIdx = currentPlayerIdx)
-
-
         Spacer(modifier = Modifier.weight(0.1f))
 
         val rightPlayer = gameState.players[(currentPlayerIdx + 1) % 4]
-
-        JassComposables.PlayerBox(player = rightPlayer, playerSpot = 1, Modifier
-            .fillMaxHeight(0.5F)
-            .fillMaxWidth(0.25F)
-            .weight(1f))
+        JassComposables.PlayerBox(
+            player = rightPlayer,
+            playerSpot = 1, Modifier
+                .fillMaxHeight(0.5F)
+                .fillMaxWidth(0.25F)
+                .weight(1f)
+        )
     }
 }
 
