d6b1e22780fe699b40fb680b4c8b14988f8033b7
LucaS <luca.engel@epfl.ch>
2023-06-20 11:11:30 +0200

    adapt hand cards displaying

--- app/src/main/java/com/github/lucaengel/jass_entials/game/JassComposables.kt
+++ app/src/main/java/com/github/lucaengel/jass_entials/game/JassComposables.kt
@@ -2,12 +2,13 @@ package com.github.lucaengel.jass_entials.game
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredHeight
 import androidx.compose.foundation.layout.requiredWidth
@@ -25,6 +26,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.github.lucaengel.jass_entials.data.cards.Card
 import com.github.lucaengel.jass_entials.data.cards.Player
+import kotlin.math.absoluteValue
 
 class JassComposables {
 
@@ -34,66 +36,86 @@ class JassComposables {
 
             val screenWidth = LocalConfiguration.current.screenWidthDp.dp
 
-            Row(
-                Modifier.fillMaxWidth()
-                    .width(screenWidth),
-                horizontalArrangement = Arrangement.Center,
-                verticalAlignment = Alignment.Bottom
-            ) {
+//            Row(
+//                Modifier
+//                    .fillMaxWidth()
+//                    .width(screenWidth),
+////                horizontalArrangement = Arrangement.Center,
+//                verticalAlignment = Alignment.Bottom
+//            ) {
 
                 val edgeSpace = screenWidth.value / 10 / 2f
 
-                val cardWidth = screenWidth.value / 10 //* 1.5f
+                val cardWidth = screenWidth.value / 10 * 1.5f
                 val cardHeight = cardWidth * 1.5f
 
+                val nbCards = player.cards.size
+                val displacements = (0 until nbCards)
+                    .map { (it - (nbCards / 2)).absoluteValue * (cardWidth * 1.2) }// - if (nbCards % 2 == 0) cardWidth / 2 else 0f }
 
-                val adjustments = listOf(
-                    edgeSpace + cardWidth/2,
-                    -cardWidth*0.5f + edgeSpace + cardWidth/2,
-                    -cardWidth*1f + edgeSpace + cardWidth/2,
-                    -cardWidth*1.5f + edgeSpace + cardWidth/2,
-                    -cardWidth*2f + edgeSpace + cardWidth/2,
-                    -cardWidth*2.5f + edgeSpace + cardWidth/2,
-                    -cardWidth*2.5f + cardWidth/2,
-                    -cardWidth*2f - edgeSpace + cardWidth/2,
-                    -cardWidth*1.5f - edgeSpace + cardWidth/2,
-                )
+                println("displacements: $displacements")
+                BoxWithConstraints(
+                    contentAlignment = Alignment.BottomCenter,
+                ) {
+                    player.cards.mapIndexed { idx, card ->
+                        Row {
+                            if (idx > nbCards / 2) Spacer(modifier = Modifier.width(displacements[idx].dp))
 
-                val middle = (player.cards.size / 2f) - 0.5f
-//        player.cards.zip(adjustments).mapIndexed { idx, (card, xTranslate) ->
-                player.cards.mapIndexed { idx, card ->
-                    // index in the list of cards
-                    var cardTranslationX = 0f
-                    var cardTranslationY = 0f
+                            Column {
+                                Spacer(modifier = Modifier.height(((idx - nbCards / 2).absoluteValue * (cardHeight / 9f)).dp))
 
-                    Box(
-                        modifier = Modifier
-                            .graphicsLayer {
-                                rotationZ = (idx - middle) * 5f
-                                translationY = (idx - middle)*(idx - middle)*0.05f*cardHeight
-//                        translationX = cardTranslationX
-                                translationX = /*xTranslate*/(idx - middle) * cardWidth * (-0.5f)
+                                Image(
+                                    painter = painterResource(id = Card.getCardImage(card)),
+                                    contentDescription = card.toString(),
+                                    modifier = Modifier
+                                        .requiredHeight((cardHeight).dp)
+                                        .requiredWidth(cardWidth.dp)
+                                        .graphicsLayer {
+                                            rotationZ = (idx - (nbCards / 2f)) * 5f
+                                        },
+                                )
                             }
-                            .requiredWidth(cardWidth.dp)
-                            .requiredHeight(cardHeight.dp),
-//                    .background(
-//                        MaterialTheme.colorScheme.primaryContainer,
-//                        shape = RoundedCornerShape(10.dp)
-//                    ),
-                        contentAlignment = Alignment.BottomCenter
+                            if (idx <= nbCards / 2) Spacer(modifier = Modifier.width(displacements[idx].dp))
 
-                    ) {
-                        Image(
-                            painter = painterResource(id = Card.getCardImage(card)),
-                            contentDescription = card.toString(),
-                            modifier = Modifier
-                                .fillMaxHeight()
-                                .align(Alignment.BottomCenter)
-                                .fillMaxSize()
-                        )
+                        }
                     }
                 }
-            }
+
+//                val middle = (player.cards.size / 2f) - 0.5f
+////        player.cards.zip(adjustments).mapIndexed { idx, (card, xTranslate) ->
+//                player.cards.mapIndexed { idx, card ->
+//                    // index in the list of cards
+//                    var cardTranslationX = 0f
+//                    var cardTranslationY = 0f
+//
+//                    Box(
+//                        modifier = Modifier
+//                            .graphicsLayer {
+//                                rotationZ = (idx - middle) * 5f
+//                                translationY = (idx - middle)*(idx - middle)*0.05f*cardHeight
+////                        translationX = cardTranslationX
+//                                translationX = /*xTranslate*/(idx - middle) * cardWidth * (-0.5f)
+//                            }
+//                            .requiredWidth(cardWidth.dp)
+//                            .requiredHeight(cardHeight.dp),
+////                    .background(
+////                        MaterialTheme.colorScheme.primaryContainer,
+////                        shape = RoundedCornerShape(10.dp)
+////                    ),
+//                        contentAlignment = Alignment.BottomCenter
+//
+//                    ) {
+//                        Image(
+//                            painter = painterResource(id = Card.getCardImage(card)),
+//                            contentDescription = card.toString(),
+//                            modifier = Modifier
+//                                .fillMaxHeight()
+//                                .align(Alignment.BottomCenter)
+//                                .fillMaxSize()
+//                        )
+//                    }
+//                }
+//            }
 //    PlayerBox(player = player, bettingState = bettingState, playerSpot = playerSpot)
         }
 
