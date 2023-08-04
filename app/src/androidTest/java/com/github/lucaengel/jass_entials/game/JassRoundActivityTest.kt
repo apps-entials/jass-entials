8abb6e03c67f7069985f36ab819cb4d8ab2c2ede
LucaS <luca.engel@epfl.ch>
2023-08-04 13:13:31 +0200

    add elements for initial screen content test

--- app/src/androidTest/java/com/github/lucaengel/jass_entials/game/JassRoundActivityTest.kt
+++ app/src/androidTest/java/com/github/lucaengel/jass_entials/game/JassRoundActivityTest.kt
@@ -2,13 +2,16 @@ package com.github.lucaengel.jass_entials.game
 
 import android.content.Intent
 import androidx.compose.ui.test.junit4.createEmptyComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
 import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.github.lucaengel.jass_entials.data.game_state.GameStateHolder
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.lang.Thread.sleep
 
 @RunWith(AndroidJUnit4::class)
 class JassRoundActivityTest {
@@ -25,6 +28,18 @@ class JassRoundActivityTest {
             composeTestRule.onNodeWithText("first_2 second_2").assertExists()
             composeTestRule.onNodeWithText("first_3 second_3").assertExists()
             composeTestRule.onNodeWithText("first_4 second_4").assertExists()
+
+            sleep(3000)
+            // current player's cards
+            val currentPlayer = GameStateHolder.gameState.playerDatas[GameStateHolder.gameState.currentPlayerIdx]
+            currentPlayer.cards.forEach {
+                composeTestRule.onNodeWithContentDescription(it.toString()).assertExists()
+            }
+
+            val trickCards = GameStateHolder.gameState.currentTrick.playerToCard.map { it.first }
+            trickCards.forEach {
+                composeTestRule.onNodeWithContentDescription(it.toString()).assertExists()
+            }
         }
     }
 }
--- app/src/main/java/com/github/lucaengel/jass_entials/data/game_state/GameStateHolder.kt
+++ app/src/main/java/com/github/lucaengel/jass_entials/data/game_state/GameStateHolder.kt
@@ -10,10 +10,10 @@ class GameStateHolder {
 
     companion object {
         private val shuffledDeck = Deck.STANDARD_DECK.shuffled()
-        private val playerData1 = PlayerData("email_1", 0, "first_1", "second_1", Deck.sortPlayerCards(shuffledDeck.cards.subList(0, 9)), 0, "123")
-        private val playerData2 = PlayerData("email_2", 0, "first_2", "second_2", Deck.sortPlayerCards(shuffledDeck.cards.subList(9, 18)), 0, "123")
-        private val playerData3 = PlayerData("email_3", 0, "first_3", "second_3", Deck.sortPlayerCards(shuffledDeck.cards.subList(18, 27)), 0, "123")
-        private val playerData4 = PlayerData("email_4", 0, "first_4", "second_4", Deck.sortPlayerCards(shuffledDeck.cards.subList(27, 36)), 0, "123")
+        private val playerData1 = PlayerData("email_1", 0, "first_1", "second_1", Deck.sortPlayerCards(shuffledDeck.cards.subList(4, 12)), 0, "123")
+        private val playerData2 = PlayerData("email_2", 0, "first_2", "second_2", Deck.sortPlayerCards(shuffledDeck.cards.subList(12, 20)), 0, "123")
+        private val playerData3 = PlayerData("email_3", 0, "first_3", "second_3", Deck.sortPlayerCards(shuffledDeck.cards.subList(20, 28)), 0, "123")
+        private val playerData4 = PlayerData("email_4", 0, "first_4", "second_4", Deck.sortPlayerCards(shuffledDeck.cards.subList(28, 36)), 0, "123")
         private val players = listOf(playerData1, playerData2, playerData3, playerData4)
 
 
@@ -23,7 +23,7 @@ class GameStateHolder {
             playerData1,
             playerData3,
             1,
-            Trick(Deck.STANDARD_DECK.cards.subList(0, 2).mapIndexed { index, card -> Pair(card, players[index]) }),
+            Trick(shuffledDeck.cards.subList(0, 4).mapIndexed { index, card -> Pair(card, players[index]) }),
             listOf(),
             1,
             Trump.UNGER_UFE,
